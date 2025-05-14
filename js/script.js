9ec6d85ae5fa974a1c834f351de9726ad9e1cedb
Rebeca Oliveira <rebecaoli.veleiros@gmail.com>
2025-05-14 16:06:58 -0300

    update portfolio

--- js/script.js
+++ js/script.js
@@ -79,7 +79,17 @@
 
         }) 
     } 
+    //Isotope - Biblioteca para Filtro
+    window.addEventListener("load", () =>   {
+        let portfolioContainer = select(".portfolio-container");
+        if (portfolioContainer) {
+            let portfolioIsotope = new Isotope(portfolioContainer,{
+                itemsSelector: '.portfolio-item'
+            });
 
+            let portfolioFilters = select('#portfolio-filters li', true);
+        }
+    });
 
 
 
